46a2337436961b2f46d1b28cb43ad84947bcc073
PrLamkin <shiektheimmortal@outlook.com>
2020-10-22 19:09:40 -0500

    minor mistake hopefully

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express")
 const fs = require("fs")
 
 var app = express();
-var PORT = process.env.PORT || 3080
+var PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use("/assets/api-routes")(app)
+app.use("/assets", express.static("./assets"))
 
 require("./routing/html-routes")(app)
 require("./routing/api-routes")(app)
